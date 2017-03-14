3b5271e5078a3b681a7480e00fb1b450ede9b203
evansc443 <evansc443@yahoo.com>
2017-03-14 14:12:15 -0400

    latest Update

--- DartSim/Course.cpp
+++ DartSim/Course.cpp
@@ -2,14 +2,18 @@
 #include "stdafx.h"
 
 Course::Course() {
-	
+	professor = "Unknown";
+	set_event_location("Unknown");
+	set_event_name("Unkown");
+	DateAndTime dt(0, 0);
+	vector<DateAndTime> dts;
+	dts.insert(dts.end(), dt);
+	set_event_times(dts);
 }
 
 Course::Course(string nam, string loc, vector<DateAndTime> dts, string prof)
 {
-	Professor x;
-	x.setName(prof);
-	professor = x;
+	professor = prof;
 	set_event_name(nam);
 	set_event_location(loc);
 	set_event_times(dts);
